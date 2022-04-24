432c3d2b757b5b504fac98fc6cd707bfc9038f71
Charles Clark <charles.clark@stonybrook.edu>
2022-04-24 19:00:12 -0400

    finished main game

--- wordle_game.cpp
+++ wordle_game.cpp
@@ -15,19 +15,40 @@ int main() {
     }
 
     string answer = rand_word(dict); // generate answer
-    cout << answer << endl;
 
-    // input guess
-    string guess{};
     cout << "Guess the word:" << endl;
-    cin >> guess; 
 
-    // check that guess is the correct length
-    if (guess.length() != answer.length()) {
-        cout << "guesses must be " << answer.length() << " characters long, enter new guess" << endl;
+    int n_guesses = 6; // number of guesses
+    int round = 1; // game round
+    while (round <= n_guesses){
+        
+        // input guess
+        string guess{};
+        cin >> guess; 
+
+        // check that guess is the correct length
+        if (guess.length() != answer.length()) {
+            cout << "guesses must be " << answer.length() << " letters long, enter new guess" << endl;
+            continue;
+        }
+
+        else {
+            color_string output = compare(guess, answer);
+            // cout << round << " ";
+            cout << output << endl;
+        }
+
+        if (guess == answer) {
+            cout << "Congratulations! You found the word" << endl;
+            break;
+        }
+
+        if (round == n_guesses) {
+            cout << "Sorry, the word was " << answer << endl;
+        }
+
+        round++;
+
     }
     
-    color_string output = compare(guess, answer);
-    cout << output << endl;
-
 }
