61cdfb037da4dfb62aea538b23f50ff7a1cbaae6
Charles Clark <charles.clark@stonybrook.edu>
2022-04-24 00:26:47 -0400

    added compare guess/answer function

--- wordle_game.cpp
+++ wordle_game.cpp
@@ -8,27 +8,25 @@ using namespace std;
 
 int main() {
 
-    vector<string> dict = get_dict();
+    vector<string> dict = get_dict(); // read in dictionary
     
     if (dict.empty()){
         return 1; // return 1 if there was error opening dictionary.txt
     }
 
-    string answer = rand_word(dict);
+    string answer = rand_word(dict); // generate answer
     cout << answer << endl;
 
+    // input guess
     string guess{};
     cout << "Guess the word:" << endl;
-    cin >> guess;
+    cin >> guess; 
 
     if (guess.length() != answer.length()) {
         cout << "guesses must be " << answer.length() << " characters long, enter new guess" << endl;
     }
-
-    // cout << "\033[" << 34 << "m" << "bold red text" << "\033[0m\n";    
-    // output x;
-    // x.characters = "hello";
-    // x.colors = {31,31,33,31,31};
-    // cout << x << endl;
+    
+    color_string output = compare(guess, answer);
+    cout << output << endl;
 
 }
