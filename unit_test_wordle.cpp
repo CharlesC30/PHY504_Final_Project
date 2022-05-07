64cf87f82856ca6fb346284eea471eb09ed277e3
Charles Clark <charles.clark@stonybrook.edu>
2022-05-07 16:17:11 -0400

    unit test for get_counts and compare

--- unit_test_wordle.cpp
+++ unit_test_wordle.cpp
@@ -5,6 +5,7 @@
 int main() {
 
     // test rand_word
+
     vector<string> test_dict {"hello", "world", "testy"};
 
     cout << "testing random word generation" << endl;
@@ -12,8 +13,31 @@ int main() {
     auto word = rand_word(test_dict);
 
     // assert that the random word is in the test dictionary
-    assert(find(test_dict.begin(), test_dict.end(), word) == test_dict.end()); 
+    assert(find(test_dict.begin(), test_dict.end(), word) != test_dict.end()); 
 
-    assert(1 == 0);
+    // test get_counts
+
+    cout << "testing get counts" << endl;
+
+    string test_str{"hello_world"};
+    auto test_charcounts = get_counts(test_str);
+    
+    vector<char> real_char{'h','e','l','o','_','w','r','d'};
+    vector<int> real_counts{1,1,3,2,1,1,1,1};
+    
+    assert((test_charcounts.unique_char == real_char) && (test_charcounts.counts == real_counts));    
+
+    // test comparison of guess and answer
+
+    cout << "testing guess-answer comparison" << endl;
+
+    string test_guess{"hello"};
+    string test_answer{"world"};
+
+    auto test_comp = compare(test_guess, test_answer);
+
+    vector<int> real_colors{90,90,90,32,33};
+
+    assert(test_comp.colors == real_colors);
 
 }
