ef6e8abc55a1f1ab7cc7508843f7c4bec4add67e
Charles Clark <charles.clark@stonybrook.edu>
2022-05-06 23:09:40 -0400

    organized colorstring funcs

---
+++ print_colorstring.cpp
@@ -0,0 +1,13 @@
+#include "ColorString.H"
+
+ostream& operator<< (ostream& os, const color_string& cs) {
+    /* define output operator for color strings so they print intuitively*/
+
+    assert(cs.characters.size() == cs.colors.size());
+
+    for (int i = 0; i < cs.characters.length(); i++) {
+        os << "\033[1;" << cs.colors[i] << "m" << cs.characters[i] << "\033[0m";
+    }
+
+    return os;
+}
